2f545c746b50ea86fa28ffdce8357160638ebf5d
LewisRKeeble <lewiskeeble98@gmail.com>
2024-06-08 16:24:46 +0100

    Adjusting projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Weatherapp from "../images/Weatherapp.PNG";
-import Lkattire from "../images/Lkattire.PNG";
+import Noteit from "../images/noteit.png";
+import Behindthebarbell from "../images/Behindthebarbell.png";
 import { TbBrandGithub, TbExternalLink } from "react-icons/tb";
 
 export default function Projects() {
@@ -11,25 +12,32 @@ export default function Projects() {
         <div className="projects-grid">
           <div className="project-card">
             <div className="project-image">
-              <img className="project-img" src={Lkattire}></img>
+              <img
+                className="project-img"
+                src={Noteit}
+                alt="project image"
+              ></img>
             </div>
             <div className="project-text">
-              <h2>LK Attire</h2>
+              <h2>Noteit</h2>
               <p>
-                A full-stack e-commerce shop for mens smart wear. This project
-                is a work in progress, but will allow the user to browse the
-                shop, login/register and order.
+                A full stack project for note taking. It allows the user to log
+                in and sign up with authentication. Users can also add edit and
+                delete notes. This app is hosted with AWS.
               </p>
               <div className="projects-stack">
                 <p className="p-text">React</p>
                 <p className="p-text">Node.js</p>
-
+                <p className="p-text">Postgres</p>
               </div>
               <div className="project-links">
-                <a href="#" className="project-button">
+                <a
+                  href="https://github.com/LewisRKeeble/Noteit?tab=readme-ov-file"
+                  className="project-button"
+                >
                   Code <TbBrandGithub />
                 </a>
-                <a href="#" className="project-button">
+                <a href="http://3.8.177.87:3000/" className="project-button">
                   Live Demo <TbExternalLink />
                 </a>
               </div>
@@ -38,60 +46,27 @@ export default function Projects() {
           <div className="project-card">
             <div className="project-image">
               <a>
-                <img></img>
+                <img className="project-img" src={Behindthebarbell}></img>
               </a>
             </div>
             <div className="project-text">
-              <h2>Title</h2>
-              <p>Description</p>
+              <h2>Behind the Barbell</h2>
+              <p>
+                A project made for a client starting her personal training
+                company. It is a multi front end page. I am currently finalising
+                parts with the client so isnt hosted but will be in the coming
+                months.
+              </p>
               <div className="projects-stack">
-                <p>React</p>
-                <p>Css</p>
+                <p className="p-text">React</p>
+                <p className="p-text">Node.js</p>
               </div>
-              <div className="projects-links">
-                <a href="#"></a>
-                <a href="#"></a>
+              <div className="project-links">
+                
               </div>
             </div>
           </div>
-          <div className="project-card">
-            <div className="project-image">
-              <a>
-                <img></img>
-              </a>
-            </div>
-            <div className="project-text">
-              <h2>Title</h2>
-              <p>Description</p>
-              <div className="projects-stack">
-                <p>React</p>
-                <p>Css</p>
-              </div>
-              <div className="projects-links">
-                <a href="#"></a>
-                <a href="#"></a>
-              </div>
-            </div>
-          </div>
-          <div className="project-card">
-            <div className="project-image">
-              <a>
-                <img></img>
-              </a>
-            </div>
-            <div className="project-text">
-              <h2>Title</h2>
-              <p>Description</p>
-              <div className="projects-stack">
-                <p>React</p>
-                <p>Css</p>
-              </div>
-              <div className="projects-links">
-                <a href="#"></a>
-                <a href="#"></a>
-              </div>
-            </div>
-          </div>
+
           <div className="project-card">
             <div className="project-image">
               <img className="project-img" src={Weatherapp}></img>
@@ -101,18 +76,19 @@ export default function Projects() {
               <p>
                 My first weather app using weatherapi.com. It is a simple
                 displayed app that takes any user input location and gives them
-                the current and two more days of weather
+                the current and two more days of weather. Did not make live due
+                to API key becoming paid for.
               </p>
               <div className="projects-stack">
-                <p className="p-text">Node.js</p>
-                <p className="p-text">EJS</p>
+                <p className="p-text">React</p>
+                <p className="p-text">API</p>
               </div>
               <div className="project-links">
-                <a href="#" className="project-button">
+                <a
+                  href="https://github.com/LewisRKeeble/WeatherIt"
+                  className="project-button"
+                >
                   Code <TbBrandGithub />
-                </a>
-                <a href="#" className="project-button">
-                  Live Demo <TbExternalLink />
                 </a>
               </div>
             </div>
